43d2d94b1acffcd330f383b10a5f4b7392d48b68
Josiah Simon-Anyebe <130301168+JosiahSimonAnyebe@users.noreply.github.com>
2024-06-24 21:38:41 +0100

    Update Admin.java

--- Java/Admin.java
+++ Java/Admin.java
@@ -1,14 +1,14 @@
-package Java;
-
 public class Admin extends User {
     String staffNo;
+
     void uploadResults(String course, float score) {
         System.out.println(course + " " + score);
     }
+
     public static void main(String[] args) {
         Admin adminName = new Admin();
-        adminName.name = "Fori";
-        adminName.password = "88888";
+        adminName.name = "Emeka";
+        adminName.password = "246810";
         System.out.println(adminName.register());
     }
-} 
+}
